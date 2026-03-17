7c7e55bad6e90d513272674516acf162091718f8
Ivaan Singh <ivaansingh.k@gmail.com>
2026-03-17 16:34:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
 
-// homepage
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome</h1>");
-});
-
 app.get("/", (req, res) => {
   let msg = req.query.msg || "Welcome";
 
