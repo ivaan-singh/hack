2997c53ab5fa49d967003d4cfcbe96550c3159d5
Ivaan Singh <ivaansingh.k@gmail.com>
2026-03-17 15:58:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const path = require("path");
+const express = require("express");
+const app = express();
 
+// homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.send("<h1>Home</h1>");
 });
+
+// dashboard
+app.get("/dashboard", (req, res) => {
+  res.send("<h1>Secret Dashboard 😈</h1>");
+});
+
+// IMPORTANT for Render
+app.listen(process.env.PORT || 3000);
