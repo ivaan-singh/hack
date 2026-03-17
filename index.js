825061a462999f5b2ed0640d2ecb3a5be88c5d2c
Ivaan Singh <ivaansingh.k@gmail.com>
2026-03-17 15:51:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-const express = require("express");
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-
-// fake user
-const realUser = {
-  username: "admin",
-  password: "1234"
-};
-
-// homepage (login)
 app.get("/", (req, res) => {
+  const msg = req.query.msg || "Welcome to the site";
+
   res.send(`
-    <h1>Login</h1>
-    <form method="POST" action="/login">
-      <input name="username" placeholder="username"/><br/>
-      <input name="password" type="password" placeholder="password"/><br/>
-      <button>Login</button>
-    </form>
+    <h1>${msg}</h1>
+    <p>Home page</p>
   `);
 });
-
-// login logic (INTENTIONALLY WEAK)
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  if (username == realUser.username && password == realUser.password) {
-    res.redirect("/dashboard");
-  } else {
-    res.send("Login failed");
-  }
-});
-
-// dashboard (no protection 👀)
-app.get("/dashboard", (req, res) => {
-  res.send(`
-    <h1>Welcome Admin</h1>
-    <p>Secret: The password is 1234</p>
-  `);
-});
-
-app.listen(3000);
