6bf9e964fffd73d58ba9169c46525936797adc10
Ivaan Singh <ivaansingh.k@gmail.com>
2026-03-17 16:02:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const app = express();
 
 // homepage
 app.get("/", (req, res) => {
-  res.send("<h1>Home</h1>");
+  res.send(`
+    <h1>Welcome</h1>
+    <p>Try to find the secret...</p>
+  `);
 });
 
-// dashboard
-app.get("/dashboard", (req, res) => {
-  res.send("<h1>Secret Dashboard 😈</h1>");
+// login route
+app.get("/login", (req, res) => {
+  const pass = req.query.password;
+
+  if (pass === "dragon123") {
+    res.send("<h1>ACCESS GRANTED 😈</h1>");
+  } else {
+    res.send("<h1>ACCESS DENIED ❌</h1>");
+  }
 });
 
-// IMPORTANT for Render
+// hidden route (not linked anywhere)
+app.get("/secret", (req, res) => {
+  res.send("<p>Password is somewhere in the code...</p>");
+});
+
 app.listen(process.env.PORT || 3000);
