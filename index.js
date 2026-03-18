c3aa0b4f91ec0509805d1c3e67d7d8dce391e126
Ivaan Singh <ivaansingh.k@gmail.com>
2026-03-18 12:57:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  let msg = req.query.msg || "Welcome";
+// 🔥 persistent message (in memory)
+let savedMessage = "<h1>Welcome</h1>";
 
-  if (msg === "[html]") {
-    msg = "<h1 style='color:red'>HACKED 😈</h1>";
+app.get("/", (req, res) => {
+  const msg = req.query.msg;
+
+  // if someone sends a message → save it
+  if (msg) {
+    savedMessage = msg;
   }
 
-  res.send(`<div>${msg}</div>`);
+  res.send(`
+    <html>
+      <head>
+        <title>Final Boss 😈</title>
+      </head>
+      <body>
+        ${savedMessage}
+      </body>
+    </html>
+  `);
 });
 
 // REQUIRED for Render
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running 😈");
+});
